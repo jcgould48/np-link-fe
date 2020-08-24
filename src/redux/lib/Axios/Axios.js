5d0eb98492af2629927b9cac178af5f6e9836a64
Jesse Gould <jesse.gould@codeimmersives.com>
2020-08-24 10:43:29 -0400

    edits Axios.js

--- src/redux/lib/Axios/Axios.js
+++ src/redux/lib/Axios/Axios.js
@@ -1,8 +1,7 @@
 import axios from "axios";
 
 const Axios = axios.create({
-  baseURL:
-    process.env.NODE_ENV === "development" ? "http://localhost:3001/" : "/api",
+  baseURL: "/api",
   timeout: 50000,
 });
 
